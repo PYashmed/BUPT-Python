33046f8495dc50bf15743a11d9687e8f7ac7be10
PYashmed <15840913883@163.com>
2022-04-03 17:35:18 +0800

    numerical calculation

---
+++ Python/PythonCrashCourse/002/number.py
@@ -0,0 +1,17 @@
+#
+# ^numerical calculation method is the same as cpp
+# ^except some like: ** /
+
+# ^ ** power
+a = 2**3  # 8
+print(0.01**0.5)  # 0.1
+
+# ^ in python 2+,operator "/" as division operator is the same as cpp,3/2=1
+# ^ in python 3+, in most time,python give the ans you want. 3/2=1.5
+print(3/2)  # 1.5
+# ^ operator "//" is exact division
+print(5//2)  # 2
+print(3//2)  # 1
+
+# ^ "%" remainder
+print(5 % 2)  # 1
